66c223ec887fb7fe3571ad8efe47d76583dca262
3171997 <zferdous35@gmail.com>
2017-11-13 01:26:57 -0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -10,31 +10,60 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', 'appController@getHomePage');
+Route::get('/home', 'appController@home');
+Route::get('/adminhome', 'AdminController@index');
+Route::get('/circular', 'appController@circular');
+Route::get('/circular/ViewCircular/Finance&Accounts', 'appController@financeAnDaccountes');
+Route::get('/circular/ViewCircular/Programming&Testing','appController@ProgrammingAnDTesting');
+Route::get('/circular/ViewCircular/Graphics&Design', 'appController@GraphicsAnDDesign');
+Route::get('/circular/ViewCircular/Management&HR', 'appController@ManagementAnDHR');
+Route::get('/circular/ViewCircular/Planning&analysis', 'appController@PlanningAnDanalysis');
+Route::get('/circular/ViewCircular', 'appController@ViewCircular');
+Route::get('/circular/ViewCircular/Apply', 'appController@apply');
+Route::get('/welcome' , function()
+           {
+               return view('welcome');
+           });
+Route::get('/alert' , function()
+           {
+               return view('alert');
+           });
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/users/logout', 'Auth\loginController@userLogout')->name('user.logout');
+Route::get('/circular/ViewCircular/Finamce&accpuntCV', 'appController@FinamceANDaccpuntCV');
+Route::get('/circular/ViewCircular/InterviewCard', 'appController@GetCard');
+Route::get('/circular/ViewCircular/Graphics&designingCV', 'appController@GraphicsANDdesigningCV');
+Route::get('/circular/ViewCircular/Management&HRCV', 'appController@ManagementANDHRCV');
+Route::get('/circular/ViewCircular/Planning&analysisCV', 'appController@PlanningANDanalysisCV');
+Route::get('/circular/ViewCircular/Programming&testingCV', 'appController@ProgrammingANDtestingCV');
+Route::post('/cv', ['uses'=>'cvController@postCv' , 'as'=>'cv']);
+Route::post('/programmingcv', ['uses'=>'ProgrammingCvController@postProgrammingCv' , 'as'=>'programmingcv']);
+Route::post('/planningcv', ['uses'=>'PlanningAndAnalysisCvController@postPlanningCv' , 'as'=>'planningcv']);
 
-Route::prefix('admin')->group(function(){
+Route::get('/submit', ['uses'=>'CvController@getShow', 'as'=>'submit']);
+Route::get('/submitprogrammingcv', ['uses'=>'ProgrammingCvController@getShowProgrammingCv', 'as'=>'submitprogrammingcv']);
+Route::get('/submitfinancecv', ['uses'=>'financeCvController@getShowFinanceCv', 'as'=>'submitfinancecv']);
+Route::post('/financecv', ['uses'=>'financeCvController@postFinanceCv' , 'as'=>'financecv']);
+
+
+Auth::routes();
+//Route::get('/admin', 'AdminController@index');
+Route::get('/homepage', ['uses'=>'appController@getHomePage', 'as'=>'homepage']);
+
+
+Route::get('/showprogrammingcv' , ['uses'=>'ProgrammingCvController@showProgrammingCv', 'as'=>'showprogrammingcv']);
+Route::get('/showfinancecv' , ['uses'=>'FinanceCvController@showFinanceCv', 'as'=>'showfinancecv']);
+Route::get('/admindashboard', ['uses'=>'appController@adminDashboard' , 'as'=>'admindashboard']);
+Route::get('/showplanningcv' , ['uses'=>'planningAndAnalysisCvController@showplanningAndAnalysisCv', 'as'=>'showplanningcv']);
+
+//Admin login
+Route::prefix('admin')->group(function()
+{
 Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+Route::post('/login', 'Auth\AdminLoginController@Login')->name('admin.login.submit');
 Route::get('/', 'AdminController@index')->name('admin.dashboard');
-Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
-Route::post('/password/email','Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
-Route::get('/password/reset','Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-Route::post('/password/reset','Auth\AdminResetPasswordController@reset');
-Route::get('/password/reset/{token}','Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
-});
 
 
-Route::get('/', function(){
-return view('welcome');
-});
-
-Route::get('/test', function(){
-return view('test');
-});
-
-Route::get('/khus', function(){
-return view('khus');
 });
 
