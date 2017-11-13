da47460e0955671139199f8ea2238b2b4df25e17
shahina38 <31255641+shahina38@users.noreply.github.com>
2017-11-13 14:04:46 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -10,53 +10,31 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'appController@home');
-Route::get('/Home', 'appController@home<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', 'appController@getHomePage');
-Route::get('/home', 'appController@home');
-Route::get('/circular', 'appController@circular');
-Route::get('/circular/ViewCircular/Finance&Accounts', 'appController@financeAnDaccountes');
-Route::get('/circular/ViewCircular/Programming&Testing','appController@ProgrammingAnDTesting');
-Route::get('/circular/ViewCircular/Graphics&Design', 'appController@GraphicsAnDDesign');
-Route::get('/circular/ViewCircular/Management&HR', 'appController@ManagementAnDHR');
-Route::get('/circular/ViewCircular/Planning&analysis', 'appController@PlanningAnDanalysis');
-Route::get('/circular/ViewCircular', 'appController@ViewCircular');
-Route::get('/circular/ViewCircular/Apply', 'appController@apply');
-Route::get('/welcome' , function()
-           {
-               return view('welcome');
-           });
 Auth::routes();
-Route::get('/circular/ViewCircular/Finamce&accpuntCV', 'appController@FinamceANDaccpuntCV');
-Route::get('/circular/ViewCircular/Graphics&designingCV', 'appController@GraphicsANDdesigningCV');
-Route::get('/circular/ViewCircular/Management&HRCV', 'appController@ManagementANDHRCV');
-Route::get('/circular/ViewCircular/Planning&analysisCV', 'appController@PlanningANDanalysis');
-Route::get('/circular/ViewCircular/Programming&testingCV', 'appController@ProgrammingANDtestingCV');
-Route::post('/cv', ['uses'=>'cvController@postCv' , 'as'=>'cv']);
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/users/logout', 'Auth\loginController@userLogout')->name('user.logout');
 
-Route::get('/submit', ['uses'=>'CvController@getShow', 'as'=>'submit']);
+Route::prefix('admin')->group(function(){
+Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+Route::get('/', 'AdminController@index')->name('admin.dashboard');
+Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+Route::post('/password/email','Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::get('/password/reset','Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('/password/reset','Auth\AdminResetPasswordController@reset');
+Route::get('/password/reset/{token}','Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+});
 
-Auth::routes();
 
-Route::get('/admin', 'AdminController@index');
-Route::get('/homepage', ['uses'=>'appController@getHomePage', 'as'=>'homepage']);
-');
-Route::get('/circular', 'appController@circular');
-Route::get('/circular/Finance&Accounts', 'appController@financeAnDaccountes');
-Route::get('/circular/Programming&Testing','appController@ProgrammingAnDTesting');
-Route::get('/circular/Graphics&Design', 'appController@GraphicsAnDDesign');
-Route::get('/circular/Management&HR', 'appController@ManagementAnDHR');
-Route::get('/circular/Planning&analysis', 'appController@PlanningAnDanalysis');
+Route::get('/', function(){
+return view('welcome');
+});
+
+Route::get('/test', function(){
+return view('test');
+});
+
+Route::get('/khus', function(){
+return view('khus');
+});
+
