f3bc247b31de2e2e8232718bae33e37b29c45142
3171997 <zferdous35@gmail.com>
2017-11-13 01:24:36 -0800

    Add files via upload

--- app/Http/Controllers/appController.php
+++ app/Http/Controllers/appController.php
@@ -7,18 +7,18 @@ use App\User;
 use App\Http\Controllers\Controller;
 class appController extends Controller
 {
-    public function home()
+    public function getHomePage()
     {
         return view('html.homepage');
+    }
+    public function home()
+    {
+        return view('html.circularlist');
     }
     public function circular()
     {
          return view('html.circular');
     }
-    /*public function show($click)
-    {
-         return view('html.circular');
-    }*/
     public function financeAnDaccountes()
     {
         return view('html.finance&accounts');
@@ -38,5 +38,44 @@ class appController extends Controller
     public function PlanningAnDanalysis()
     {
         return view('html.Planning&analysis');
+    }
+    public function ViewCircular()
+    {
+        return view('html.circularlist');
+    }
+    public function FinamceANDaccpuntCV()
+    {
+        return view('html.Finamce&accpuntCV');
+    }
+    public function GraphicsANDdesigningCV()
+    {
+        return view('html.Graphics&designingCV');
+    }
+    public function ManagementANDHRCV()
+    {
+        return view('html.Management&HRCV');
+    }
+    public function PlanningANDanalysisCV()
+    {
+        return view('html.Planning&analysisCV');
+    }
+    public function ProgrammingANDtestingCV()
+    {
+        return view('html.Programming&testingCV');
+    }
+    public function GetCard()
+    {
+        $cards =  ProgrammingCv::all();
+        return view('html.card', ['cards'=>$cards]);
+    }
+    /*public function apply()
+    {
+        return view('html.CV');
+    }*/
+
+
+    public function adminDashboard()
+    {
+        return view('admin.programming&testing');
     }
 }
